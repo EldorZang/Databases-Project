2d0a165a1caf46ebf13ba70e4c9cc1b31cc7fbc4
Eldor Zang <e.z.eldor.zang@gmail.com>
2024-02-14 23:06:40 +0200

    Added login page and main menu

---
+++ Demo of a site/dbconnection.py
@@ -0,0 +1,20 @@
+import mysql.connector
+
+def connect_db():
+    connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="123456",
+        database="FunGeo"
+    )
+    return connection
+
+def execute_query(query, params=None):
+    connection = connect_db()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute(query, params)
+    result = cursor.fetchall()
+
+    connection.close()
+    return result
